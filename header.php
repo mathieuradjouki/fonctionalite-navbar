95d0f9a3dc2406ceeedaac3504e5d61311bc8c6d
mathieuradjouki <m.radjouki@codeur.online>
2020-08-31 12:02:38 +0200

    add commentaire

--- header.php
+++ header.php
@@ -15,14 +15,26 @@
           <a href="#">Css |</a>
           <a href="#">Javascript</a>
       </div>
-      <div class="burger-menu-open">
+      <div class="burger-menu-open" id="burgerNav" onclick="openNav()">
         <div class="bar1"></div>
         <div class="bar2"></div>
         <div class="bar3"></div>
       </div>
-      <div class="burger-menu-close">
-        <div class="close-bar1"></div>
-        <div class="close-bar2"></div>
-        <div class="close-bar3"></div>
+      <div class="menu-close">
+        <div id="myNav" class="overlay">
+          <a href="#" onclick="closeNav()">
+            <div class="burger-menu-close">
+              <div class="close-bar1"></div>
+              <div class="close-bar2"></div>
+              <div class="close-bar3"></div>
+            </div>
+          </a>
+          <div class="overlay-content">
+            <a href="#">Accueil</a>
+            <a href="#">Html</a>
+            <a href="#">Css</a>
+            <a href="#">Javascript</a>
+          </div>
+        </div>
       </div>
     </header>
---
+++ main.js
@@ -0,0 +1,10 @@
+// open on some click on burger menu
+function openNav(){
+  document.getElementById("myNav").style.display = "block";
+  document.getElementById("burgerNav").style.display = "none";
+}
+// close on some click on close button
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+  document.getElementById("burgerNav").style.display = "block";
+}
