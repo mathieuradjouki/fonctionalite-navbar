1a7345f07daede14d4219729d61351097fa1bb14
mathieuradjouki <m.radjouki@codeur.online>
2020-08-31 09:14:38 +0200

    création base du menu

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ma navbar</title>
+    <link rel="stylesheet" href="main.css">
+    <script type="text/javascript" src="main.js"></script>
+  </head>
+  <body>
+    <header>
+      <div class="navbar">
+        <ul>
+          <li>Accueil</li>
+          <li>Html</li>
+          <li>Css</li>
+          <li>Javascript</li>
+        </ul>
+      </div>
+    </header>
