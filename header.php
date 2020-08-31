1069d8120a6d6f309ce1a50abbdcfbfa6eec03b4
mathieuradjouki <m.radjouki@codeur.online>
2020-08-31 10:07:52 +0200

    media menu burger

--- header.php
+++ header.php
@@ -3,17 +3,26 @@
   <head>
     <meta charset="utf-8">
     <title>Ma navbar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="main.css">
     <script type="text/javascript" src="main.js"></script>
   </head>
   <body>
     <header>
       <div class="navbar">
-        <ul>
-          <li>Accueil</li>
-          <li>Html</li>
-          <li>Css</li>
-          <li>Javascript</li>
-        </ul>
+          <a href="#">Accueil |</a>
+          <a href="#">Html |</a>
+          <a href="#">Css |</a>
+          <a href="#">Javascript</a>
+      </div>
+      <div class="burger-menu-open">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+      </div>
+      <div class="burger-menu-close">
+        <div class="close-bar1"></div>
+        <div class="close-bar2"></div>
+        <div class="close-bar3"></div>
       </div>
     </header>
